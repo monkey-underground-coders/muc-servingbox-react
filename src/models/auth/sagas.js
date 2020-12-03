914f60669838d7adf3ce163d7fdfb26ca5361eb3
Gleb Potapov <gleb.potapov@saltpepper.ru>
2020-12-04 00:15:32 +0300

    Implement token saving

--- src/models/auth/sagas.js
+++ src/models/auth/sagas.js
@@ -3,17 +3,20 @@ import { put, takeEvery, call } from "redux-saga/effects";
 import * as api from "api";
 import { actions } from "./slice";
 import { generatebase64 } from "utils/genBase64";
+import { setLocalStorage } from "utils/localStorage";
 
 export function* fetchTokens(action) {
   const encodedData = generatebase64(action.payload.data);
 
   try {
     const response = yield call(api.getTokens, encodedData);
-
+    const tokens = response.data;
     yield put({
       type: actions.getTokensSuccess,
       payload: { tokens: response.data },
     });
+    setLocalStorage("access", tokens.accessToken);
+    setLocalStorage("refresh", tokens.refreshToken);
   } catch (err) {
     yield put({
       type: actions.getTokensFailed,
