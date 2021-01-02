6b6388ce80b9eb789e7896028d5eb58a89b6e30b
senketsukamui <glebpotapov2001@gmail.com>
2021-01-02 21:18:24 +0300

    Change lesson page render logic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import Header from "components/Header/Header";
 import Sidebar from "components/Sidebar/Sidebar";
 import useAction from "hooks/useAction";
+import { authFetchingSelector } from "models/auth/selectors";
 import { actions } from "models/auth/slice";
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import styles from "./Home.module.scss";
 import Main from "./Main/Main";
 
 const Home = () => {
   const getAccess = useAction(actions.getAccess);
+  const fetching = useSelector(authFetchingSelector);
   useEffect(() => {
     console.log("effect");
     getAccess();
@@ -17,7 +20,7 @@ const Home = () => {
       <Sidebar />
       <div className={styles.main}>
         <Header title="Уроки" />
-        <Main />
+        {!fetching && <Main />}
       </div>
     </div>
   );
